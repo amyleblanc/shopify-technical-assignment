7e1eb14be20d702048af05b88aeabdc16427e5b2
amyleblanc <amymccarthy2014@gmail.com>
2022-05-16 15:50:39 -0700

    create component to map through & list responses

---
+++ pages/components/ResponseList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useHistoryContext } from '../contexts/useHistoryContext';
+import Response from './Response';
+
+export default function ResponseList() {
+  const { history } = useHistoryContext();
+
+  return (
+    <>
+      {history.map(element => (
+        <Response question={element.question} answer={element.answer} key={element.id}/>
+      ))}
+    </>
+  );
+}
