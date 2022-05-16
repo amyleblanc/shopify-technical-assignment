06f5bdb78238c40a0de9a3b93bdb1748e6336bf5
amyleblanc <amymccarthy2014@gmail.com>
2022-05-16 16:54:00 -0700

    format code; add background colour to Card

--- pages/components/Response.jsx
+++ pages/components/Response.jsx
@@ -5,12 +5,24 @@ import Typography from '@mui/material/Typography';
 
 export default function Response(props) {
   return (
-    <Card sx={{maxWidth: 500, maxHeight: 350, padding: "10px", marginLeft: "2em", marginBottom: "2em"}}>
+    <Card sx={{
+      maxWidth: "500px", 
+      maxHeight: "400px", 
+      padding: "10px", 
+      marginLeft: "2em", 
+      marginBottom: "2em", 
+      backgroundColor: "#458f47", 
+      boxShadow: 3,
+    }}>
       <CardContent>
-        <Typography variant="h6" component="div" sx={{pb: "10px"}}>
+        <Typography variant="h6" component="div" 
+          sx={{
+            pb: "10px", 
+            color: "white",
+          }}>
           {props.question}
         </Typography>
-        <Typography variant="h7">
+        <Typography variant="h7" sx={{color: "white"}}>
           {props.answer}
         </Typography>
       </CardContent>
