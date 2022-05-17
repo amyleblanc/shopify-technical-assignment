75f7b4237b0a95cdb9ebf9976aabfac23f4bc6d7
amyleblanc <amymccarthy2014@gmail.com>
2022-05-17 13:46:34 -0700

    create function to filter history for empty string

--- components/ResponseList.jsx
+++ components/ResponseList.jsx
@@ -5,9 +5,20 @@ import Response from './Response';
 export default function ResponseList() {
   const { history } = useHistoryContext();
 
+  const filterHistory = (array) => {
+    const filtered = [];
+    for (const obj of array) {
+      if (obj.answer !== "") {
+        filtered.push(obj);
+      }
+    }
+    return filtered;
+  }
+  const responses = filterHistory(history);
+
   return (
     <>
-      {history.map(element => (
+      {responses.map(element => (
         <Response 
           question={element.question}
           answer={element.answer} 
