866ed48d8365e5ffa01795dff8d6a2a5e1fa26a7
amyleblanc <amymccarthy2014@gmail.com>
2022-05-17 13:06:15 -0700

    include initial value for history state

--- contexts/useHistoryContext.js
+++ contexts/useHistoryContext.js
@@ -5,7 +5,11 @@ const HistoryContext = createContext();
 export const useHistoryContext = () => useContext(HistoryContext);
 
 export const HistoryProvider = props => {
-  const [history, setHistory] = useState([]);
+  const [history, setHistory] = useState([{
+    id: 123,
+    question: "",
+    answer: "",
+  }]);
 
   return (
     <HistoryContext.Provider value={{ history, setHistory }}>
